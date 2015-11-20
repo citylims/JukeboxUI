c6afa6c6deb7d0281ccf84d74c238da1a1306ed3
Ausin Formica <austin.formica@gmail.com>
2015-11-20 12:58:49 -0500

    fetch gradients

--- app/js/controllers/juke_controller.js
+++ app/js/controllers/juke_controller.js
@@ -1,10 +1,38 @@
-app.controller('JukeCtrl', function($scope, Spotify) {
+app.controller('JukeCtrl', JukeCtrl)
+
+JukeCtrl.$inject['$scope', "$http", "Spotify"];
+
+function JukeCtrl($scope, $http, Spotify) {
+
+  init();
+
+  function init(){
+    fetchGradients();
+  }
 
   $scope.searchArtist = function () {
+    console.log($scope.gradients);
     Spotify.search($scope.searchartist, 'artist').then(function (data) {
       console.log($scope.artists);
       $scope.artists = data.artists.items;
     });
   };
 
-});
+ function fetchGradients() {
+    $http.get('assets/gradients.json').success(function(data){
+      console.log(data);
+      $scope.gradients = data;
+    })
+  }
+
+  $scope.next = function() {
+    console.log("yup");
+    fetchGradients();
+  }
+
+  var shuffle = function(o) {
+    for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+    return o;
+  }
+
+};
