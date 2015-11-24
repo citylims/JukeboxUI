e4a24c38702ea330724c17d994129491e8b725ec
Ausin Formica <austin.formica@gmail.com>
2015-11-24 16:11:10 -0500

    add transition refresh

--- app/js/services/juke_service.js
+++ app/js/services/juke_service.js
@@ -5,12 +5,13 @@
     .module('jukeboxApp')
     .factory('JukeService', JukeService);
 
-  JukeService.$inject = ['$http'];
+  JukeService.$inject = ['$http', '$timeout'];
 
-  function JukeService($http) {
+  function JukeService($http, $timeout) {
     return {
       getGradients: getGradients,
-      getPlaylist: getPlaylist
+      getPlaylist: getPlaylist,
+      transitionRefresh: transitionRefresh
     };
 
     function getGradients() {
@@ -21,6 +22,22 @@
       return $http.get('js/playlist.json');
     }
 
-  }
+    function transitionRefresh(pallete, gradients) {
+      pallete.toggleClass('load')
+      var gradients = shuffle(gradients);
+
+      $timeout(function(){
+        pallete.toggleClass('load');
+        var gradient = gradients[0];
+        pallete.css("background", "linear-gradient(to left, " + gradient.colors[0] + ", " + gradient.colors[1] + ")");
+      }, 800) //
+
+      function shuffle(o) {
+        for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        return o;
+      }
+    }
+
+  }//JukeService
 
 })();
