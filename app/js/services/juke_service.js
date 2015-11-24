c72293f468f0b97fbffe508ff790847a1f2e7e5e
Ausin Formica <austin.formica@gmail.com>
2015-11-24 15:13:17 -0500

    get playlists

--- app/js/services/juke_service.js
+++ app/js/services/juke_service.js
@@ -9,11 +9,16 @@
 
   function JukeService($http) {
     return {
-      getGradients: getGradients
+      getGradients: getGradients,
+      getPlaylist: getPlaylist
     };
 
     function getGradients() {
       return $http.get('assets/gradients.json');
+    }
+
+    function getPlaylist() {
+      return $http.get('js/playlist.json');
     }
 
   }
