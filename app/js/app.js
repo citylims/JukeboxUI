4f077908dff89a105f1e76fb0d38dd1d44fded3b
Ausin Formica <austin.formica@gmail.com>
2015-11-24 14:07:49 -0500

    define config for ui.router

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,22 @@
+(function() {
 'use strict';
 
-var app = angular.module('jukeboxApp', ['spotify', 'ui.router'])
-app.config(config);
+  var app = angular.module('jukeboxApp', ['spotify', 'ui.router'])
+  app.config(config);
 
-function config(SpotifyProvider, $stateProvider, $urlRouterProvider) {
-  SpotifyProvider.setClientId('27abb617a029486f9665b4a0a0c6adfe');
-  SpotifyProvider.setRedirectUri('http://localhost:8888/callback.html');
-  SpotifyProvider.setScope('playlist-read-private');
+  function config(SpotifyProvider, $stateProvider, $urlRouterProvider) {
+    SpotifyProvider.setClientId('27abb617a029486f9665b4a0a0c6adfe');
+    SpotifyProvider.setRedirectUri('http://localhost:8888/callback.html');
+    SpotifyProvider.setScope('playlist-read-private');
 
-  $stateProvider
+    $stateProvider
 
-  .state('jukebox', {
-    url: '/jukebox',
-    templateUrl: 'templates/jukebox.html',
-    controller: 'JukeCtrl'
-  })
+    .state('jukebox', {
+      url: '/jukebox',
+      templateUrl: 'templates/jukebox.html',
+      controller: 'JukeCtrl'
+    })
 
-  //next
-
-  //previous
-
-  $urlRouterProvider.otherwise('/jukebox');
-};
+    $urlRouterProvider.otherwise('/jukebox');
+  };
+})();
