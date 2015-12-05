f9d4acf9e636ce67d6c1eb42bb06982f14d8c34a
Ausin Formica <austin.formica@gmail.com>
2015-12-04 23:01:32 -0500

    update dependencies

--- app/js/controllers/juke_controller.js
+++ app/js/controllers/juke_controller.js
@@ -8,7 +8,7 @@ angular
 
   JukeCtrl.$inject['$scope', "$http", "Spotify", "JukeService"];
 
-  function JukeCtrl($scope, $http, Spotify, $timeout, $sce, JukeService) {
+  function JukeCtrl($scope, $http, Spotify, JukeService) {
 
     init();
 
@@ -16,17 +16,17 @@ angular
       fetchPlaylist()
     }
 
-    $scope.searchArtist = function () {
-      Spotify.search($scope.searchartist, 'artist').then(function (data) {
-        $scope.artists = data.artists.items;
-      });
-    };
-
     function fetchPlaylist() {
       JukeService.getPlaylist().then(function(res) {
         $scope.playlist = res.data.tracks.items;
       });
     }
+
+    $scope.searchArtist = function () {
+      Spotify.search($scope.searchartist, 'artist').then(function (data) {
+        $scope.artists = data.artists.items;
+      });
+    };
 
   };//JukeCtrl
 })();
